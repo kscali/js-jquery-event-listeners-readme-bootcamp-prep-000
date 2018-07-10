588a756227c35e192643d26f73fbaac60ca498f8
kosatun@aol.com <kosatun@aol.com>
2018-07-10 18:47:42 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -16,7 +16,15 @@ $(document).ready(function(){
   
   function pressIt() {
     $(document).on('keydown', function(key) {
-      if(key.which == )
-    }
-  }
+      if (key.which == 71) {
+        alert('G was pressed.');
+      }
+    });
+   }
+   
+   function submitIt() {
+     $("form").on("submit", function() {
+       alert("Your form is going to be submitted now.");
+     });
+     }
 });
